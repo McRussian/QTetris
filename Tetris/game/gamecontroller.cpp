a8479443a0acead365618f1e28a6292b961ca38a
McRussian Andrey <mcrussianaa@gmail.com>
2022-05-11 23:05:12 +0500

    Add Init Variant for GameController

--- Tetris/game/gamecontroller.cpp
+++ Tetris/game/gamecontroller.cpp
@@ -2,5 +2,97 @@
 
 GameController::GameController()
 {
+    this->_initFigures();
+    this->_initColors();
+}
+
+GameController::~GameController()
+{
+    delete this->_game_field;
+}
+
+QMap<QChar, QString> GameController::getColors()
+{
+    return this->_colors;
+}
+
+QVector<QString> GameController::getField()
+{
+    return this->_game_field->getField();
+}
+
+void GameController::createGame(unsigned int width, unsigned int height)
+{
+    this->_game_width = width;
+    this->_game_height = height;
+    this->_game_field = new GameField(width, height);
+}
+
+void GameController::_initFigures()
+{
+    QVector<QString> line;
+    line.push_back(QString('x'));
+    line.push_back(QString('x'));
+    line.push_back(QString('x'));
+    line.push_back(QString('x'));
+    this->_patterns.push_back(line);
+
+    QVector<QString> square;
+    square.push_back(QString("xx"));
+    square.push_back(QString("xx"));
+    this->_patterns.push_back(square);
+
+    QVector<QString> triangle;
+    triangle.push_back(QString(" x "));
+    triangle.push_back(QString("xxx"));
+    this->_patterns.push_back(triangle);
+
+    QVector<QString> table;
+    table.push_back(QString("xxx"));
+    table.push_back(QString("x x"));
+    this->_patterns.push_back(table);
+
+    QVector<QString> zigzag_l;
+    zigzag_l.push_back(QString("xx "));
+    zigzag_l.push_back(QString(" xx"));
+    this->_patterns.push_back(zigzag_l);
+
+    QVector<QString> zigzag_r;
+    zigzag_r.push_back(QString(" xx"));
+    zigzag_r.push_back(QString("xx "));
+    this->_patterns.push_back(zigzag_r);
+
+    QVector<QString> line_l;
+    line_l.push_back(QString("x "));
+    line_l.push_back(QString("x "));
+    line_l.push_back(QString("xx"));
+
+    QVector<QString> line_r;
+    line_r.push_back(QString(" x"));
+    line_r.push_back(QString(" x"));
+    line_r.push_back(QString("xx"));
+    this->_patterns.push_back(line_l);
+    this->_patterns.push_back(line_r);
+
+    QVector<QString> square_big;
+    square_big.push_back(QString("xxx"));
+    square_big.push_back(QString("xxx"));
+    square_big.push_back(QString("xxx"));
+    this->_patterns.push_back(square_big);
+
+}
+
+void GameController::_initColors()
+{
+    this->_colors['g'] = "#95FAC0";
+    this->_colors['b'] = "#7DFBFB";
+    this->_colors['p'] = "#EFB2FB";
+    this->_colors['r'] = "#F7282B";
+    this->_colors['d'] = "#9D9D9D";
+    this->_colors['y'] = "#EBFB72";
+}
+
+Figure GameController::_createRandomFigure()
+{
 
 }
--- Tetris/game/gamecontroller.h
+++ Tetris/game/gamecontroller.h
@@ -2,12 +2,34 @@
 #define GAMECONTROLLER_H
 
 #include <QObject>
+#include <QVector>
+#include <QMap>
+
+#include "game/figure.h"
+#include "game/gamefield.h"
 
 class GameController
 {
     Q_OBJECT
 public:
     GameController();
+    ~GameController();
+
+    QMap<QChar, QString> getColors();
+    QVector<QString> getField();
+    void createGame(unsigned int, unsigned int);
+
+private:
+    void _initFigures();
+    void _initColors();
+    Figure _createRandomFigure();
+
+private:
+    unsigned int _game_width;
+    unsigned int _game_height;
+    QVector<QVector<QString>> _patterns;
+    QMap<QChar, QString> _colors;
+    GameField* _game_field;
 };
 
 #endif // GAMECONTROLLER_H
